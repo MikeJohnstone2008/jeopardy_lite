e17c34ec5a2239bbbdfdb958fa0c10b190f6417f
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-08-25 15:53:41 -0700

    almost complete; still needs tweeking

--- assets/java_script/javascript.js
+++ assets/java_script/javascript.js
@@ -1,13 +1,52 @@
-<script>
   (function(){
 
     $(function(){
 
-      $.get("http://jservice.io/api/random", function(data){
+      let answer;
+      let question;
+      let value;
+      let category;
+      let submit_btn;
+      let total = 0;
 
+      function getQuestion(){
+        $.get("http://jservice.io/api/random", function(data){
+          answer = data[0].answer
+          question = data[0].question
+          value = data[0].value
+          category = data[0].category.title
+          // $("#question_returned").html(data[0].question)
+          $("#answer_returned").html(answer)
+          $("#question_returned").html(question)
+          $("#value_returned").html(value)
+          $("#category_returned").html(category)
+          console.log(answer)
+          console.log(question)
+          console.log(value)
+          console.log(category)
+        })
+      }
+
+      getQuestion();
+
+      //button click, basic-addon1, make visibility: visible
+      $("#basic-addon1" ).click(function() {
+
+        submit_btn = $("#basic-addon1").val();
+        $("#answer_returned").show();
+        total = total + value;
+        $("#score_tl").html(total);
+        console.log($("#userAnswer").val())
+        if(answer.toLowerCase() === $("#userAnswer").val().toLowerCase()){
+          console.log("you rule")
+        }else{
+          console.log("you drool")
+        }
+      });
+      // $("")   button glick=-get next question
+      $("#new_question" ).click(function() {
+        getQuestion();
       })
-
     })
 
   })()
-</script>
